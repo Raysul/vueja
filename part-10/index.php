6251d5e8dceb55ed1c93bb4a7b31cb48d61a5221
Md.Raysul kobir <mdraysulkobir283170@gmail.com>
2019-08-07 18:48:04 +0600

    w

--- part-10/index.php
+++ part-10/index.php
@@ -9,9 +9,7 @@
 
 <body>
     <div id="info" style="text-align:center;">
-        <h2>{{ name | lower }}</h2>
-        <h3>{{ address | upper }}</h3>
-        <h4>To Date: {{ today }}</h4>
+
     </div>
 
 
@@ -19,32 +17,12 @@
 
 
     <script type="text/javascript">
-    // < global function
-
-    Vue.filter("lower", function(s) {
-        return s.toLowerCase();
-    })
-
-
-
-
-    // date function filer
-
-
-
     var app = new Vue({
         el: "#info",
         data: {
-            name: "Hello World",
-            address: "My address In jamalpur",
-            today: new Date(),
-        },
 
-        filters: {
-            upper: function(s) {
-                return s.toUpperCase();
-            }
         }
+
     });
     </script>
 </body>
