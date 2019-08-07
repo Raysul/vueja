c0ed36b68ef709c5f5750b821336784bc5312a06
Md.Raysul kobir <mdraysulkobir283170@gmail.com>
2019-08-07 17:47:50 +0600

    global function in filber

--- part-10/index.php
+++ part-10/index.php
@@ -9,13 +9,23 @@
 
 <body>
     <div id="info" style="text-align:center;">
-        <h2>{{ name | upper }}</h2>
+        <h2>{{ name | lower }}</h2>
         <h3>{{ address | upper }}</h3>
     </div>
 
 
 
+
+
     <script type="text/javascript">
+    // < global function
+
+    Vue.filter("lower", function(s) {
+        return s.toLowerCase();
+    })
+
+
+
     var app = new Vue({
         el: "#info",
         data: {
