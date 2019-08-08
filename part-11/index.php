9c7cfc40e8726aef13b13bdd9e0bd10609e21f80
Md.Raysul kobir <mdraysulkobir283170@gmail.com>
2019-08-08 12:05:34 +0600

    show an if show workd but show display in non and if content remode

---
+++ part-11/index.php
@@ -0,0 +1,49 @@
+<html>
+
+<head>
+    <title>Part 11</title>
+    <script src="js/vue.js"></script>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+        integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
+        integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+        integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous">
+    </script>
+</head>
+
+
+
+<body>
+    <div id="info" style="text-align:center;">
+        <div class="container">
+            <div class="panel panel-default">
+                <div class="panel-heading">Panel Heading</div>
+                <div class="panel-body">Panel Content</div>
+            </div>
+        </div>
+    </div>
+
+<script type="text/javascript">
+    Vue.component('panel' function(){
+        
+    })
+</script>
+
+
+    <script type="text/javascript">
+    var app = new Vue({
+        el: "#info",
+        data: {
+
+        }
+    });
+    </script>
+</body>
+
+</html>
